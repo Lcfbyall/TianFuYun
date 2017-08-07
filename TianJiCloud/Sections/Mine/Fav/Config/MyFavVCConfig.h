66e9c88e5db158665c7fb3ae124b71750f3725d0
staff <staff@Ivan-MacBook-Pro.local>
2017-08-07 22:47:12 +0800

    add myfav

---
+++ TianJiCloud/Sections/Mine/Fav/Config/MyFavVCConfig.h
@@ -0,0 +1,13 @@
+//
+//  MyFavVCConfig.h
+//  TianJiCloud
+//
+//  Created by staff on 2017/8/7.
+//  Copyright © 2017年 TianJiMoney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyFavVCConfig : NSObject
+
+@end
---
+++ TianJiCloud/Sections/Mine/Fav/Config/MyFavVCConfig.m
@@ -0,0 +1,13 @@
+//
+//  MyFavVCConfig.m
+//  TianJiCloud
+//
+//  Created by staff on 2017/8/7.
+//  Copyright © 2017年 TianJiMoney. All rights reserved.
+//
+
+#import "MyFavVCConfig.h"
+
+@implementation MyFavVCConfig
+
+@end
